f09312f82310dd0070315c8212f59107ce39ed16
timestep <aaronksivbalan@gmail.com>
2013-06-10 10:01:22 -0400

    New Method, added IRC Break/Mute

--- irc_bot.rb
+++ irc_bot.rb
@@ -2,27 +2,37 @@ require "socket"
 
 server = "chat.freenode.net"
 port = "6667"
-nick = "AliceBot"
-@channel = "#alicebot"
+@nick = "CopyCat"
+@channel = "#bitmaker"
 @prefix = "PRIVMSG #{@channel} :"
 # greeting = ["hello", "hi", "hola", "yo"]
 
 # opens a new socket with the correct port and server
 @irc_server = TCPSocket.open(server, port)
-
-@irc_server.puts "USER AliceBot 0 * Testing"
-@irc_server.puts "NICK #{nick}"
+@irc_server.puts "USER CopyCat 0 * Testing"
+@irc_server.puts "NICK #{@nick}"
 @irc_server.puts "JOIN #{@channel}"
 # @irc_server.puts "#{@prefix} " # use this if I want to say something in the channel
+
+def msg_to_server(msg)
+	@irc_server.puts msg
+end
 
 def simple(msg)
 	a = msg.partition(@prefix)
 	return a[2]
 end
 
-
 def search_users_input(msg)
-	if msg.include? "#{@prefix}"
+	if msg.include? "#{@nick}: Mute"
+		msg_to_server("#{@prefix}Muted")
+		while true
+			msg = @irc_server.gets
+  			puts msg
+  			break if msg.include?("#{@nick}: Run")
+  		end
+		msg_to_server("#{@prefix}YOLOGETWRKD")
+	elsif msg.include? "#{@prefix}"
 		b = simple(msg)
 		@irc_server.puts "#{@prefix}#{b}"
 	end	
@@ -31,8 +41,8 @@ end
 until @irc_server.eof? do
   msg = @irc_server.gets
   puts msg
-  puts search_users_input(	msg)
-  # @irc_server.puts "#{@prefix} #{msg}"
+  puts search_users_input(msg)
+		# @irc_server.puts "#{@prefix} #{msg}"
 end
 
 
